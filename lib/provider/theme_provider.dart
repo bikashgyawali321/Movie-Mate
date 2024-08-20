b7f48d4caee58ff31a408ab6cdc378f652eb30e0
Bikash <gyawalibikash321@gmail.com>
2024-08-20 20:21:16 +0545

    hive added and shared preferences added

--- lib/provider/theme_provider.dart
+++ lib/provider/theme_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
@@ -12,28 +11,9 @@ class ThemeProvider extends ChangeNotifier {
   void setTheme(ThemeMode themeMode) async {
     _themeMode = themeMode;
     notifyListeners();
-    final prefs = await SharedPreferences.getInstance();
-    prefs.setString("themeMode", themeMode.toString().split('.').last);
   }
 
   void _loadThemeMode() async {
-    final prefs = await SharedPreferences.getInstance();
-    final storedThemeMode = prefs.getString('themeMode');
-    if (storedThemeMode != null) {
-      switch (storedThemeMode) {
-        case "light":
-          _themeMode = ThemeMode.light;
-          break;
-        case "dark":
-          _themeMode = ThemeMode.dark;
-          break;
-        case "system":
-        default:
-          _themeMode = ThemeMode.system;
-      }
-    } else {
-      _themeMode = ThemeMode.system;
-    }
     notifyListeners();
   }
 }
